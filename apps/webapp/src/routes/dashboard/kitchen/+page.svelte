ac6b263fb4ab739caf5b361cca79fcdd057f59b2
rayan147 <rayan361@gmail.com>
2025-02-19 14:57:50 -0500

    add paginations

--- apps/webapp/src/routes/dashboard/kitchen/+page.svelte
+++ apps/webapp/src/routes/dashboard/kitchen/+page.svelte
@@ -3,9 +3,19 @@
   import { Search } from 'lucide-svelte';
   import * as Table from '$lib/components/ui/table';
   import type { PageProps } from './$types';
+  import * as Pagination from '$lib/components/ui/pagination';
+  import { page } from '$app/state';
+  import { goto } from '$app/navigation';
 
   let { data }: PageProps = $props();
-  console.log(data);
+  const searchTerm = page.url.searchParams.get('search') || '';
+
+  function handleSearch() {
+    const params = new URLSearchParams();
+    if (searchTerm) params.get('search', searchTerm);
+    params.set('page', '1');
+    goto(`?${params.toString()}`);
+  }
 </script>
 
 <section class="w-full flex justify-center mt-10">
@@ -18,24 +28,57 @@
 <!-- Table components -->
 <section class="mt-6">
   <Table.Root>
-    <Table.Caption>A list of your recent recipes.</Table.Caption>
+    <Table.Caption>A list of your top recent recipes.</Table.Caption>
     <Table.Header>
       <Table.Row>
         <Table.Head class="w-[100px]">Name</Table.Head>
-        <Table.Head>Category</Table.Head>
-        <Table.Head>Menu</Table.Head>
-        <Table.Head class="text-right">Image</Table.Head>
+        <Table.Head>Cook Time</Table.Head>
+        <Table.Head>Image</Table.Head>
+        <Table.Head class="text-right">Cost</Table.Head>
       </Table.Row>
     </Table.Header>
     <Table.Body>
-      {#each data.recipes as { id, name }, i (id)}
+      {#each data.recipes as { id, name, cookTime, imageUrl, description }, i (id)}
         <Table.Row>
           <Table.Cell class="font-medium">{name}</Table.Cell>
-          <Table.Cell>Paid</Table.Cell>
-          <Table.Cell>Credit Card</Table.Cell>
+          <Table.Cell>{cookTime} mins</Table.Cell>
+          <Table.Cell
+            ><img
+              src={imageUrl}
+              class="rounded-md object-cover h-10"
+              alt={description}
+            /></Table.Cell
+          >
           <Table.Cell class="text-right">$250.00</Table.Cell>
         </Table.Row>
       {/each}
     </Table.Body>
   </Table.Root>
+</section>
+
+<!-- Pagination -->
+<section class="mt-6">
+  <Pagination.Root count={100} perPage={10} let:pages let:currentPage>
+    <Pagination.Content>
+      <Pagination.Item>
+        <Pagination.PrevButton />
+      </Pagination.Item>
+      {#each pages as page (page.key)}
+        {#if page.type === 'ellipsis'}
+          <Pagination.Item>
+            <Pagination.Ellipsis />
+          </Pagination.Item>
+        {:else}
+          <Pagination.Item isVisible={currentPage == page.value}>
+            <Pagination.Link {page} isActive={currentPage == page.value}>
+              {page.value}
+            </Pagination.Link>
+          </Pagination.Item>
+        {/if}
+      {/each}
+      <Pagination.Item>
+        <Pagination.NextButton />
+      </Pagination.Item>
+    </Pagination.Content>
+  </Pagination.Root>
 </section>
--- apps/webapp/src/routes/dashboard/kitchen/+page.ts
+++ apps/webapp/src/routes/dashboard/kitchen/+page.ts
@@ -1,10 +1,23 @@
 import { api } from "@ckm/lib-api";
 import type { PageLoad } from "./$types";
 import { error } from '@sveltejs/kit';
-export const load: PageLoad = async ({ params }) => {
-  try {
 
-    const { status, body } = await api.recipe.getRecipes({});
+
+export const load: PageLoad = async ({ url, params }) => {
+  try {
+    // The formula is: skip = (page - 1) * perPage
+    const page = parseInt(url.searchParams.get('page') || '1')
+    const perPage = 10;
+    const skip = (page - 1) * perPage;
+    const searchTerm = url.searchParams.get('search') || '';
+
+    const { status, body } = await api.recipe.getRecipes({
+      query: {
+        skip,
+        take: perPage,
+        searchTerm: searchTerm || undefined,
+      }
+    });
     if (status !== 200) error(400, { message: `Not found` })
 
     return body
